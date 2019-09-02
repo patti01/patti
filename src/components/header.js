50066380804a22c62dda9bd9d23dbab16f10d233
Patricia Falandysz <pfalandysz@yahoo.com>
2019-09-02 13:58:17 +0200

    setup done

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <div className="HeaderBrand">
+        <Link to="/"><img src={require('../images/logo-bauhaus-black.png')} width="30"/></Link>
+      </div>
+      <div className="HeaderNav">
+        <Link to="/works" className="menu-item">Works</Link>
+        <Link to="/playground" className="menu-item">Playground</Link>
+        <Link to="/contact"><button>Contact</button></Link>
+      </div>
     </div>
   </div>
 )
